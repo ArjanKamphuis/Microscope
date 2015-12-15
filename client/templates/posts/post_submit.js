607ea9fd5879f987d4ea67c1fdec2737846b62db
ArjanKamphuis <arjan_kamphuis@hotmail.com>
2015-12-15 11:07:56 +0100

    7-5-1 Demonstrate the order that posts appear using a sleep

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -10,7 +10,7 @@ Template.postSubmit.events({
 			// display the error to the user and abort
 			if (error) { return alert(error.reason); }
 			if (result.postExists) { alert('This link has already been posted'); }
-			Router.go('postPage', { _id: result._id });
 		})
+		Router.go('postsList');
 	}
 });
